7e9c129863f730e942901a16d33de08e422e7ad3
Nick-Hericks <nickhericks@gmail.com>
2019-05-21 17:04:55 +0200

    Create app.js file

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require('express');
 
-const app = express():
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('<h1>I love Treehouse!</h1>');
+});
+
+app.get('/hello', (req, res) => {
+	res.send('<h1>Hello route!</h1>');
+});
+
+app.listen(port, () => {
+	console.log('The application is running on localhost:3000');
+});
 
