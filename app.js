0ca0683a4bca855f7e87bcb58251a9b615ada839
Nick-Hericks <nickhericks@gmail.com>
2019-05-22 07:28:00 +0200

    Add routes folder

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 /* eslint-disable no-console */
 const express = require('express');
+var sequelize = require('./models').sequelize;
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('<h1>I love Treehouse!</h1>');
-});
+const routes = require('./routes');
+
+
+app.use(routes);
+
+
 
 app.get('/hello', (req, res) => {
 	res.send('<h1>Hello route!</h1>');
 });
 
-app.listen(port, () => {
-	console.log('The application is running on localhost:3000');
+sequelize.sync().then(function() {
+	app.listen(port, () => {
+		console.log('The application is running on localhost:3000');
+	});
 });
-
