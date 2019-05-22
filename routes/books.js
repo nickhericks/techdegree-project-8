f8ce30165e50b02cf58ef3a4660a0903fc20448f
Nick-Hericks <nickhericks@gmail.com>
2019-05-22 11:43:07 +0200

    Require Book model

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+var Book = require('../models').Book;
+
+
+
+
+
 router.get('/', (req, res) => {
 	res.send('<h1>books route</h1>');
 });
